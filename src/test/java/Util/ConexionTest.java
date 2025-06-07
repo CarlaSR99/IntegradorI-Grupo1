e2cfa0a205ca67f92e6109ccf2104552f27a20a8
Carla Sandoval <sandovalrujelcarla1999@gmail.com>
2025-06-07 01:36:30 -0500

    Estructura del Proyecto

---
+++ src/test/java/Util/ConexionTest.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package Util;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author CARLA
+ */
+public class ConexionTest {
+    
+    public ConexionTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSomeMethod() {
+        fail("The test case is a prototype.");
+    }
+    
+}
